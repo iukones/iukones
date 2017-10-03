d306d99ed7c6c8ca0dc12374a8d5a6aa087dcf90
Israel Martinez <iukones1@gmail.com>
2017-10-02 19:31:05 -0500

    genera de nuevo template
    se corrige la redireccion del sitio estatido de WP

--- template_static/wp-login.php
+++ template_static/wp-login.php
@@ -9,19 +9,19 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Israel Martínez | Front-end Developer &lsaquo; Acceder</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='https://http://www.iukones.com/template/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.2' type='text/css' media='all' />
+<link rel='stylesheet' href='https://www.iukones.com/template/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.2' type='text/css' media='all' />
 <meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
-	<link rel="icon" href="https://http://www.iukones.com/template/wp-content/uploads/2017/10/cropped-iuk-final-e1506982988204-32x32.png" sizes="32x32" />
-<link rel="icon" href="https://http://www.iukones.com/template/wp-content/uploads/2017/10/cropped-iuk-final-e1506982988204-192x192.png" sizes="192x192" />
-<link rel="apple-touch-icon-precomposed" href="https://http://www.iukones.com/template/wp-content/uploads/2017/10/cropped-iuk-final-e1506982988204-180x180.png" />
-<meta name="msapplication-TileImage" content="https://http://www.iukones.com/template/wp-content/uploads/2017/10/cropped-iuk-final-e1506982988204-270x270.png" />
+	<link rel="icon" href="https://www.iukones.com/template/wp-content/uploads/2017/10/cropped-iuk-final-e1506982988204-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://www.iukones.com/template/wp-content/uploads/2017/10/cropped-iuk-final-e1506982988204-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://www.iukones.com/template/wp-content/uploads/2017/10/cropped-iuk-final-e1506982988204-180x180.png" />
+<meta name="msapplication-TileImage" content="https://www.iukones.com/template/wp-content/uploads/2017/10/cropped-iuk-final-e1506982988204-270x270.png" />
 	</head>
 	<body class="login login-action-login wp-core-ui  locale-es-mx">
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Funciona gracias a WordPress" tabindex="-1">Israel Martínez | Front-end Developer</a></h1>
 	
-<form name="loginform" id="loginform" action="https://http://www.iukones.com/template/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="https://www.iukones.com/template/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Nombre de usuario o dirección de correo electrónico<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -33,13 +33,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Recuérdame</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acceder" />
-		<input type="hidden" name="redirect_to" value="https://http://www.iukones.com/template/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="https://www.iukones.com/template/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-	<a href="https://http://www.iukones.com/template/wp-login.php?action=lostpassword">¿Has perdido tu contraseña?</a>
+	<a href="https://www.iukones.com/template/wp-login.php?action=lostpassword">¿Has perdido tu contraseña?</a>
 </p>
 
 <script type="text/javascript">
@@ -63,7 +63,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="https://http://www.iukones.com/template/">&larr; Regresar a Israel Martínez | Front-end Developer</a></p>
+	<p id="backtoblog"><a href="https://www.iukones.com/template/">&larr; Regresar a Israel Martínez | Front-end Developer</a></p>
 	
 	</div>
 
